49d146845432fd2cbacfd92cbff2c457c5cc4626
LyneaW <wangecilynea@gmail.com>
2023-07-20 02:45:12 -0700

    Updated the main.h file

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define DELIMETER " \n\t\r"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,16 +13,12 @@
 
 extern char **environ;
 
-/**
- * Declaration of functions
- */
-
+/*Declaration of functions*/
 void display_prompt(void);
 int bettyStyle(int m, int n);
 void print_env(void);
-int main(int argc, char *argv);
-void free_tokens(char **args);
-char *tokenize(char *str);
-char *location(const char *cmd);
+int main(int argc, char **argv, char **envp);
+void tokens(char **args);
+char *path(const char *cmd);
 int exec(char *argv, char **av);
 #endif
