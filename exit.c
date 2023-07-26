c5346ea0dd8fe566811c9cf46c3224091380e2f6
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-07-26 09:01:31 -0700

    simple_shell

--- exit.c
+++ exit.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include "command_path.c"
-#include "main.c"
 /**
 * exit_status - exits shell with an integer
 * Return: Always 0(Success).
@@ -17,8 +15,6 @@ int exit_status(void)
 		display_prompt();
 		if (getline(&cmd, &len, stdin) == -1)
 		{
-			free(cmd);
-			free(arg);
 			return (0);
 		}
 		arg = tokenize(cmd);
@@ -27,7 +23,9 @@ int exit_status(void)
 			if (strcmp(arg[0], "exit") == 0)
 			{
 				if (arg[1])
+				{
 					exit(atoi(arg[1]));
+				}
 				else
 					exit(0);
 			}
@@ -47,4 +45,43 @@ int exit_status(void)
 		len = 0;
 	}
 	return (0);
+}
+/**
+* getfull_path - full path of a command
+* @cmd: the command
+* Return: pointer
+*/
+char *getfull_path(const char *cmd)
+{
+	char *path;
+	char *path_env = getenv("PATH");
+	char *token;
+	int len;
+	char *full_path;
+
+	if (!path_env)
+		return (NULL);
+	path = strdup(path_env);
+	if (!path)
+		return (NULL);
+	while (token)
+	{
+		len = strlen(token) + strlen(cmd) + 2;
+		full_path = malloc(len);
+		if (!full_path)
+		{
+			free(path);
+			return (NULL);
+		}
+		printf(full_path, len, "%s%s", token, cmd);
+		if (access(full_path, F_OK) == 0)
+		{
+			free(path);
+			return (full_path);
+		}
+		free(full_path);
+		token = strtok(NULL, DELIMETER);
+	}
+	free(path);
+	return (NULL);
 }
